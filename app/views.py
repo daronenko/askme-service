e04a8300359af1fb7e08b04a19d408a679bd8812
Daniil Mironenko <dan.mironenko@gmail.com>
2024-03-30 17:23:43 +0300

    update index page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Question {i}',
+        'text': f'This is question number {i}.',
+        'tags': ['python', 'vk-education', 'postgresql', 'django', 'duckduckgo', 'mail-ru'],
+    } for i in range(5)
+]
+
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'questions': QUESTIONS, 'authorized': True})
