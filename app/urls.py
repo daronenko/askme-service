5542c0bdae6a0f69d3dbf168e89079b604373957
Daniil Mironenko <dan.mironenko@gmail.com>
2024-03-30 17:35:37 +0300

    update hot questions page

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hot/', views.hot_questions, name='hot_questions'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 
+
 QUESTIONS = [
     {
         'id': i,
         'title': f'Question {i}',
         'text': f'This is question number {i}.',
         'tags': ['python', 'vk-education', 'postgresql', 'django', 'duckduckgo', 'mail-ru'],
-    } for i in range(5)
+    } for i in range(10)
 ]
 
 
 def index(request):
     return render(request, 'index.html', {'questions': QUESTIONS, 'authorized': True})
+
+
+def hot_questions(request):
+    questions = QUESTIONS[:5]
+    return render(request, 'hot-questions.html', {'questions': questions, 'authorized': True})
