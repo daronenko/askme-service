f1724389b7c3085b2a7f689c2f4e82677f5a5c75
Daniil Mironenko <dan.mironenko@gmail.com>
2024-03-30 19:27:43 +0300

    update question details page

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ from app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hot/', views.hot_questions, name='hot_questions'),
+    path('question/<int:question_id>', views.question, name='question'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ QUESTIONS = [
         'title': f'Question {i}',
         'text': f'This is question number {i}.',
         'tags': ['python', 'vk-education', 'postgresql', 'django', 'duckduckgo', 'mail-ru'],
+        'answers': [f'Comment {j}' for j in range(5)],
     } for i in range(10)
 ]
 
@@ -18,3 +19,8 @@ def index(request):
 def hot_questions(request):
     questions = QUESTIONS[:5]
     return render(request, 'hot-questions.html', {'questions': questions, 'authorized': True})
+
+
+def question(request, question_id):
+    question = QUESTIONS[question_id]
+    return render(request, 'question-details.html', {'question': question, 'authorized': True})
