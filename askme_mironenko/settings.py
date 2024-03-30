e13a8c7baa8eab7e5ca2cebf1f30eab158ed4bd6
Daniil Mironenko <dan.mironenko@gmail.com>
2024-03-30 16:20:22 +0300

    setup index page

--- askme_mironenko/settings.py
+++ askme_mironenko/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'askme_mironenko.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 try:
     from askme_mironenko.local_settings import *
